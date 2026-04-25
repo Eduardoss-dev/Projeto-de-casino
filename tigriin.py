9cb01b12ce906a6aa89b4d0ccd8b49cad6aafa60
Eduardo Souza <eduardo46495194@gmail.com>
2026-04-24 21:57:03 -0300

    Remove project initialization commands
    Removed Git commands and initial project setup from the script.

--- tigriin.py
+++ tigriin.py
@@ -1,11 +1,3 @@
-echo "# Projeto-de-casino" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Eduardoss-dev/Projeto-de-casino.git
-git push -u origin main
-
 
 import random
 import time
